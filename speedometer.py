17d2d3e91de963c0c9208f2ee6884fee517bda4d
nmccoy <nmccoy@mit.edu>
2016-01-27 01:05:45 -0500

    added an initialization

--- speedometer.py
+++ speedometer.py
@@ -3,4 +3,11 @@ import pyaudio
 #Initialize PyAudio
 pyaud = pyaudio.PyAudio()
 
+# Open input stream, 16-bit stereo at 4410 Hz
+stream = pyaud.open(
+	format = pyaudio.paInt16,
+	channels = 2,
+	rate = 44100,
+	input_device_index = 2,
+	input = True)
  
